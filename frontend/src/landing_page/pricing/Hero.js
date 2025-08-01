c6e58b73dac076795a06da3adda481b23d7ce7f4
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-08-01 12:51:16 +0530

    pricing component added

--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <h1>Hero</h1>
-  )
-}
+    <div className="container" style={{margin: "0 auto"}}>
+      <div className="row mt-5">
+        <div className="row">
+          <div className="text-center" style={{ padding: "100px 0px" }}>
+            <h1 className="custom-h1">Charges</h1>
+            <p className="subheading">List of all charges and taxes</p>
+            <section className="row charges-section">
+              <div className="col-4 four">
+                <img src="media/images/pricingEquity.svg" />
+                <h2 className="charges-banner-heading ">Free equity delivery</h2>
+                <p className="text-gray">
+                  All equity delivery investments (NSE, BSE), are absolutely
+                  free — ₹ 0 brokerage.
+                </p>
+              </div>
+              <div className="col-4 four">
+                <img src="media/images/pricing-20.svg" />
+                <h2 className="charges-banner-heading">Intraday and F&O trades</h2>
+                <p className="text-gray">
+                  Flat ₹ 20 or 0.03% (whichever is lower) per executed order on
+                  intraday trades across equity, currency, and commodity trades.
+                  Flat ₹20 on all option trades.
+                </p>
+              </div>
+              <div className="col-4 four">
+                <img src="media/images/pricingEquity.svg" />
+                <h2 className="charges-banner-heading">Free direct MF</h2>
+                <p className="text-gray">
+                  All direct mutual fund investments are absolutely free — ₹ 0
+                  commissions & DP charges.
+                </p>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -7,6 +7,7 @@ import Footer from '../Footer'
 const PricingPage = () => {
   return (
     <>
+        <Hero />
         <Brokerage />
     </>
   )
