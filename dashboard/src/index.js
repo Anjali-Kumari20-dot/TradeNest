d0b30559dd54b254aa8a0e74b2ab72e8a359a08d
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-08-07 15:28:56 +0530

    Dashboard UI done

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import App from "./components/App";
-import Dashboard from "./components/Dashboard";
-import Funds from "./components/Funds";
-import Holdings from "./components/Holdings";
 import Home from "./components/Home";
-import Menu from "./components/Menu";
-import Orders from "./components/Orders";
-import Positions from "./components/Positions";
-import Summary from "./components/Summary";
-import TopBar from "./components/TopBar";
-import WatchList from "./components/WatchList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
